bd41399c294623586f7074053b839254589e397f
matt-stutzman <112518765+matt-stutzman@users.noreply.github.com>
2025-02-08 13:00:25 -0700

    working on franchise

--- src/tests/franchiserouter.test.js
+++ src/tests/franchiserouter.test.js
@@ -1,6 +1,18 @@
 const request = require('supertest');
 const app = require('../service');
+const {DB} = require("../database/database");
 
-test("test", async() => {
-    
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test("get all franchises", async() => {
+    const resp = await request(app).get("/api/franchise");
+    expect(resp.status).toBe(200);
+    expect(resp.body).toBeDefined();
 })
