7c2ab4261446cc4167dddba393fbc3b39a5702f6
matt-stutzman <112518765+matt-stutzman@users.noreply.github.com>
2025-03-18 23:11:33 -0600

    started deliverable 8
    hoping to get this done on Thursday at the latest

---
+++ src/metrics.js
@@ -0,0 +1,85 @@
+const os = require('os');
+const config = require('./config');
+
+/*
+
+1. http requests by method/minute
+    a. total requests
+    b. GET, PUT, POST, DELETE requests
+2. Active Users
+3. Authentication attempts/minute
+    a. successful
+    b. failed
+4.CPU and Memory usage percentage
+5.Pizzas
+    a. sold/minute
+    b. creation failures
+    c. revenue/minute
+6.Latency
+    a. service endpoint
+    b. Pizza creation
+*/
+
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+
+function sendMetricToGrafana(metricName, metricValue, type, unit) {
+    const metric = {
+      resourceMetrics: [
+        {
+          scopeMetrics: [
+            {
+              metrics: [
+                {
+                  name: metricName,
+                  unit: unit,
+                  [type]: {
+                    dataPoints: [
+                      {
+                        asDouble: metricValue,
+                        timeUnixNano: Date.now() * 1000000,
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  
+    if (type === 'sum') {
+      metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+      metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic = true;
+    }
+  
+    const body = JSON.stringify(metric);
+    fetch(`${config.url}`, {
+      method: 'POST',
+      body: body,
+      headers: { Authorization: `Bearer ${config.apiKey}`, 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          response.text().then((text) => {
+            console.error(`Failed to push metrics data to Grafana: ${text}\n${body}`);
+          });
+        } else {
+          console.log(`Pushed ${metricName}`);
+        }
+      })
+      .catch((error) => {
+        console.error('Error pushing metrics:', error);
+      });
+  }
